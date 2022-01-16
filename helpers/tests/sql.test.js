dbc712513873484eaf63fca748d7b8d393af3616
Jacob Andes <jacob.andes@gmail.com>
2022-01-15 23:08:51 -0800

    tests: added more tests, updated lint-staged config

---
+++ helpers/tests/sql.test.js
@@ -0,0 +1,45 @@
+const { sqlForPartialUpdate, sqlForPartialInsert } = require("../sql");
+
+describe("sqlForPartialUpdate", () => {
+  it("works: 1 item", () => {
+    const result = sqlForPartialUpdate({ f1: "v1" }, { f1: "f1", fF2: "f2" });
+    expect(result).toEqual({
+      setCols: '"f1"=$1',
+      values: ["v1"],
+    });
+  });
+
+  it("works: 2 items", () => {
+    const result = sqlForPartialUpdate(
+      { f1: "v1", jsF2: "v2" },
+      { jsF2: "f2" }
+    );
+    expect(result).toEqual({
+      setCols: '"f1"=$1, "f2"=$2',
+      values: ["v1", "v2"],
+    });
+  });
+});
+
+describe("sqlForPartialInsert", () => {
+  it("works: 1 item", () => {
+    const result = sqlForPartialInsert({ f1: "v1" }, { f1: "f_1" });
+    expect(result).toEqual({
+      sqlInsertCols: "f_1",
+      sqlInsertVals: "$1",
+      values: ["v1"],
+    });
+  });
+
+  it("works: 2 items", () => {
+    const result = sqlForPartialInsert(
+      { f1: "v1", f2: "v2" },
+      { f1: "f_1", f2: "f_2" }
+    );
+    expect(result).toEqual({
+      sqlInsertCols: "f_1, f_2",
+      sqlInsertVals: "$1, $2",
+      values: ["v1", "v2"],
+    });
+  });
+});
